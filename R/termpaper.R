c73736700d5d496f319201ed4384dc9414c85c3a
simazhi <thomas_van_hoey@hotmail.com>
2018-03-22 00:28:38 +0800

    change location of tex file

--- R/termpaper.R
+++ R/termpaper.R
@@ -9,7 +9,7 @@ termpaper <- function(toc = TRUE, ...) {
     fig_caption = TRUE,
 #    latex_engine = "xelatex",
     toc = toc,
-    template = latex,
+    template = "inst/rmarkdown/templates/termpaper/latex/rtemplate.tex",
     ...
     )
 
