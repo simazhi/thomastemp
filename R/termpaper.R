34b3e3b59322cf2ebc42ae0a68bcfe81ab87f5ad
simazhi <thomas_van_hoey@hotmail.com>
2018-03-22 00:08:34 +0800

    termpaper function

---
+++ R/termpaper.R
@@ -0,0 +1,16 @@
+termpaper <- function(toc = TRUE) {
+
+  # get the locations of resource files located within the package
+  latex <- system.file("/Users/Thomas/Desktop/templates/thomastemp/inst/rmarkdown/templates/termpaper/latex/rtemplate.tex", package = "thomastemp")
+#  header <- system.file("reports/quarterly/header.html", package = "mypackage")
+
+  # call the base html_document function
+  bookdown::pdf_document2(
+    fig_caption = TRUE,
+    latex_engine = xelatex,
+    toc: toc,
+    template = latex,
+    ...
+    )
+
+}
