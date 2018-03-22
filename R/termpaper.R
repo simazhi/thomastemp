2603474dfc2dacefdb8a9f4276c8770fe17a8568
simazhi <thomas_van_hoey@hotmail.com>
2018-03-22 17:17:06 +0800

    template conference_abstract

--- R/termpaper.R
+++ R/termpaper.R
@@ -14,3 +14,21 @@ termpaper <- function(toc = TRUE, ...) {
     )
 
 }
+
+
+conference_abstract <- function(toc = TRUE, ...) {
+
+  # get the locations of resource files located within the package
+  #  latex <- system.file("inst/rmarkdown/templates/termpaper/latex/rtemplate.tex", package = "thomastemp")
+  #  header <- system.file("reports/quarterly/header.html", package = "mypackage")
+
+  # call the base html_document function
+  bookdown::pdf_document2(
+    fig_caption = TRUE,
+    #    latex_engine = "xelatex",
+    toc = toc,
+    template = "confabstract.tex",
+    ...
+  )
+
+}
